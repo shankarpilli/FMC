cb2c5a3fd2b4a7810bbfa123e776a14deaa19264
manikanta.chadaram1992@gmail.com <manikanta.chadaram@sparity.com>
2016-11-05 00:14:09 +0530

    1st commit

--- app/src/main/java/com/versatilemobitech/fmc/activities/DashboardActivity.java
+++ app/src/main/java/com/versatilemobitech/fmc/activities/DashboardActivity.java
@@ -11,6 +11,7 @@ public class DashboardActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_dashboard);
     }
 }
