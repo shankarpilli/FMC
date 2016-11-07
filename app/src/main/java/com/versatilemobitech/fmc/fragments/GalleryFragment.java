a7abfa1c9a8d2034ea39cbafb0fa37a380d35419
Shankar Pilli <shankar.pilli@sparity.com>
2016-11-07 14:50:57 +0530

    Fixed alignments in gallery

--- app/src/main/java/com/versatilemobitech/fmc/fragments/GalleryFragment.java
+++ app/src/main/java/com/versatilemobitech/fmc/fragments/GalleryFragment.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
-import android.widget.LinearLayout;
 
 import com.versatilemobitech.fmc.R;
 import com.versatilemobitech.fmc.activities.DashboardActivity;
