fbcde950b6c2e1b1d586759508b7d2ac62f1575e
Manikanta Chadaram <manikanta.chadaram@sparity.com>
2016-12-08 15:56:37 +0530

    contact us is done the navigations

--- app/src/main/java/com/versatilemobitech/fmc/fragments/ContactsUsFragment.java
+++ app/src/main/java/com/versatilemobitech/fmc/fragments/ContactsUsFragment.java
@@ -1,6 +1,13 @@
 package com.versatilemobitech.fmc.fragments;
 
 
+import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,12 +19,16 @@ import android.widget.TextView;
 
 import com.versatilemobitech.fmc.R;
 import com.versatilemobitech.fmc.activities.DashboardActivity;
+import com.versatilemobitech.fmc.designs.MaterialDialog;
+import com.versatilemobitech.fmc.permissions.Permissions;
 import com.versatilemobitech.fmc.utility.Utility;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ContactsUsFragment extends Fragment {
+public class ContactsUsFragment extends Fragment implements View.OnClickListener {
 
     public static final String TAG = "ContactsUsFragment";
     private DashboardActivity mParent;
@@ -62,15 +73,20 @@ public class ContactsUsFragment extends Fragment {
 
     private void initUI() {
 
-        ll_share = (LinearLayout)rootView.findViewById(R.id.ll_share);
+        if (Utility.isMarshmallowOS()) {
+            Permissions.getInstance().setActivity(getActivity());
+            CheckForPermissions(getActivity(), Manifest.permission.CALL_PHONE);
+        }
 
-        tv_share = (TextView)rootView.findViewById(R.id.tv_share);
-        tv_contact_us = (TextView)rootView.findViewById(R.id.tv_contact_us);
-        tv_address = (TextView)rootView.findViewById(R.id.tv_address);
-        tv_mobile_num = (TextView)rootView.findViewById(R.id.tv_mobile_num);
-        tv_mail = (TextView)rootView.findViewById(R.id.tv_mail);
-        tv_web = (TextView)rootView.findViewById(R.id.tv_web);
-        tv_follow_us = (TextView)rootView.findViewById(R.id.tv_follow_us);
+        ll_share = (LinearLayout) rootView.findViewById(R.id.ll_share);
+
+        tv_share = (TextView) rootView.findViewById(R.id.tv_share);
+        tv_contact_us = (TextView) rootView.findViewById(R.id.tv_contact_us);
+        tv_address = (TextView) rootView.findViewById(R.id.tv_address);
+        tv_mobile_num = (TextView) rootView.findViewById(R.id.tv_mobile_num);
+        tv_mail = (TextView) rootView.findViewById(R.id.tv_mail);
+        tv_web = (TextView) rootView.findViewById(R.id.tv_web);
+        tv_follow_us = (TextView) rootView.findViewById(R.id.tv_follow_us);
 
         iv_location = (ImageView) rootView.findViewById(R.id.iv_location);
         iv_share = (ImageView) rootView.findViewById(R.id.iv_share);
@@ -94,5 +110,114 @@ public class ContactsUsFragment extends Fragment {
         tv_mail.setTypeface(Utility.setTypeFaceRobotoRegular(mParent));
         tv_web.setTypeface(Utility.setTypeFaceRobotoRegular(mParent));
         tv_follow_us.setTypeface(Utility.setTypeFaceRobotoRegular(mParent));
+
+        iv_location.setOnClickListener(this);
+        iv_share.setOnClickListener(this);
+        iv_linked_in.setOnClickListener(this);
+        iv_g_plus.setOnClickListener(this);
+        iv_twitter.setOnClickListener(this);
+        iv_fb.setOnClickListener(this);
+        iv_web.setOnClickListener(this);
+        iv_mail.setOnClickListener(this);
+        iv_mobile.setOnClickListener(this);
+
+        tv_share.setOnClickListener(this);
+        tv_contact_us.setOnClickListener(this);
+        tv_address.setOnClickListener(this);
+        tv_mobile_num.setOnClickListener(this);
+        tv_mail.setOnClickListener(this);
+        tv_web.setOnClickListener(this);
+        tv_follow_us.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_share:
+            case R.id.tv_share:
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "FMC");
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+                break;
+            case R.id.iv_web:
+            case R.id.tv_web:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com")));
+                break;
+            case R.id.iv_mail:
+            case R.id.tv_mail:
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                        "mailto", "daduvainaveenkumar@gmail.com", null));
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Ourtour");
+                startActivity(Intent.createChooser(emailIntent, "Send email..."));
+                break;
+            case R.id.iv_mobile:
+            case R.id.tv_mobile_num:
+                Intent intentDail = new Intent(Intent.ACTION_DIAL);
+                intentDail.setData(Uri.parse("tel:"+tv_mobile_num.getText().toString().trim()));
+                startActivity(intentDail);
+                break;
+            case R.id.iv_fb:
+                String uri = "https://www.facebook.com/ourtour.georace.1";
+                Intent fbIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                startActivity(fbIntent);
+                break;
+            case R.id.iv_g_plus:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://plus.google.com/u/0/104074863268189065180")));
+                break;
+            case R.id.iv_linked_in:
+                Intent linkedIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("linkedin://you"));
+                final PackageManager packageManager = getContext().getPackageManager();
+                final List<ResolveInfo> list = packageManager.queryIntentActivities(linkedIntent, PackageManager.MATCH_DEFAULT_ONLY);
+                if (list.isEmpty()) {
+                    linkedIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.linkedin.com/profile/view?id=you"));
+                }
+                startActivity(linkedIntent);
+                break;
+            case R.id.iv_twitter:
+                String mTwitterUrl = "https://twitter.com/ourtour9";
+                try {
+                    Intent twitterIntent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse(mTwitterUrl));
+                    startActivity(twitterIntent);
+
+                } catch (Exception e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse(mTwitterUrl)));
+                }
+                break;
+        }
+    }
+    private void CheckForPermissions(final Context mContext, final String... mPermisons) {
+        // A request for two permissions
+        Permissions.getInstance().requestPermissions(new Permissions.IOnPermissionResult() {
+            @Override
+            public void onPermissionResult(Permissions.ResultSet resultSet) {
+
+                if (resultSet.isPermissionGranted(Manifest.permission.CALL_PHONE)) {
+                    // setUi();
+                } else {
+                    final MaterialDialog denyDialog = new MaterialDialog(mContext, Permissions.TITLE,
+                            Permissions.MESSAGE);
+                    //Positive
+                    denyDialog.setAcceptButton("RE-TRY");
+                    denyDialog.setOnAcceptButtonClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            CheckForPermissions(mContext, mPermisons);
+                        }
+                    });
+                    denyDialog.show();
+                }
+            }
+
+            @Override
+            public void onRationaleRequested(Permissions.IOnRationaleProvided callback, String... permissions) {
+                Permissions.getInstance().showRationaleInDialog(Permissions.TITLE,
+                        Permissions.MESSAGE, "RE-TRY", callback);
+            }
+        }, mPermisons);
     }
 }
