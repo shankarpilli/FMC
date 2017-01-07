7fb4ac1c4193025828eafe0f33d7af9eb9991698
rahulvemula1283 <rahulvemula1283@gmail.com>
2017-01-07 15:16:02 +0530

    Changed contact us screen as per the Ourtour Loader icon is changed Navigation drawer header chagned.

--- app/src/main/java/com/versatilemobitech/fmc/fragments/ContactsUsFragment.java
+++ app/src/main/java/com/versatilemobitech/fmc/fragments/ContactsUsFragment.java
@@ -9,6 +9,7 @@ import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,169 +28,44 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ContactsUsFragment extends Fragment implements View.OnClickListener {
+public class ContactsUsFragment extends Fragment {
 
-    public static final String TAG = "ContactsUsFragment";
+    public static final String TAG = "ContctUsFragment";
     private HomeActivity mParent;
+    private String mToolBarTitle;
+    private Toolbar mToolbar;
     private View rootView;
-
-    private LinearLayout ll_share;
-
-    private TextView tv_share;
-    private TextView tv_follow_us;
-    private TextView tv_web;
-    private TextView tv_mail;
-    private TextView tv_mobile_num;
-    private TextView tv_address;
-    private TextView tv_contact_us;
-
-    private ImageView iv_location;
-    private ImageView iv_mobile;
-    private ImageView iv_mail;
-    private ImageView iv_web;
-    private ImageView iv_fb;
-    private ImageView iv_twitter;
-    private ImageView iv_g_plus;
-    private ImageView iv_linked_in;
-    private ImageView iv_share;
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mParent = (HomeActivity) getActivity();
+        mToolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
+
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         mParent.getSupportActionBar().setTitle(Utility.getResourcesString(getActivity(), R.string.contacts_us));
-        //mParent.txt_fmc.setTypeface(Utility.setTypeFaceRobotoRegular(getActivity()));
-        rootView = inflater.inflate(R.layout.fragment_contacts_us, container, false);
-        initUI();
-        return rootView;
+        if (mToolbar != null) {
+            mToolbar.setVisibility(View.VISIBLE);
+        }
+        rootView = inflater.inflate(R.layout.contact_fmc, container, false);
+        //mParent.txt_our_tour.setText("" + mToolBarTitle);
 
-    }
-
-    private void initUI() {
+        InitUI(rootView);
 
         if (Utility.isMarshmallowOS()) {
             Permissions.getInstance().setActivity(getActivity());
             CheckForPermissions(getActivity(), Manifest.permission.CALL_PHONE);
         }
 
-        ll_share = (LinearLayout) rootView.findViewById(R.id.ll_share);
-
-        tv_share = (TextView) rootView.findViewById(R.id.tv_share);
-        tv_contact_us = (TextView) rootView.findViewById(R.id.tv_contact_us);
-        tv_address = (TextView) rootView.findViewById(R.id.tv_address);
-        tv_mobile_num = (TextView) rootView.findViewById(R.id.tv_mobile_num);
-        tv_mail = (TextView) rootView.findViewById(R.id.tv_mail);
-        tv_web = (TextView) rootView.findViewById(R.id.tv_web);
-        tv_follow_us = (TextView) rootView.findViewById(R.id.tv_follow_us);
-
-        iv_location = (ImageView) rootView.findViewById(R.id.iv_location);
-        iv_share = (ImageView) rootView.findViewById(R.id.iv_share);
-        iv_linked_in = (ImageView) rootView.findViewById(R.id.iv_linked_in);
-        iv_g_plus = (ImageView) rootView.findViewById(R.id.iv_g_plus);
-        iv_twitter = (ImageView) rootView.findViewById(R.id.iv_twitter);
-        iv_fb = (ImageView) rootView.findViewById(R.id.iv_fb);
-        iv_web = (ImageView) rootView.findViewById(R.id.iv_web);
-        iv_mail = (ImageView) rootView.findViewById(R.id.iv_mail);
-        iv_mobile = (ImageView) rootView.findViewById(R.id.iv_mobile);
-
-        setTypeFace();
+        return rootView;
     }
 
-    private void setTypeFace() {
-
-        tv_share.setTypeface(Utility.setTypeFaceRobotoRegular(mParent));
-        tv_contact_us.setTypeface(Utility.setTypeFaceRobotoRegular(mParent));
-        tv_address.setTypeface(Utility.setTypeFaceRobotoRegular(mParent));
-        tv_mobile_num.setTypeface(Utility.setTypeFaceRobotoRegular(mParent));
-        tv_mail.setTypeface(Utility.setTypeFaceRobotoRegular(mParent));
-        tv_web.setTypeface(Utility.setTypeFaceRobotoRegular(mParent));
-        tv_follow_us.setTypeface(Utility.setTypeFaceRobotoRegular(mParent));
-
-        iv_location.setOnClickListener(this);
-        iv_share.setOnClickListener(this);
-        iv_linked_in.setOnClickListener(this);
-        iv_g_plus.setOnClickListener(this);
-        iv_twitter.setOnClickListener(this);
-        iv_fb.setOnClickListener(this);
-        iv_web.setOnClickListener(this);
-        iv_mail.setOnClickListener(this);
-        iv_mobile.setOnClickListener(this);
-
-        tv_share.setOnClickListener(this);
-        tv_contact_us.setOnClickListener(this);
-        tv_address.setOnClickListener(this);
-        tv_mobile_num.setOnClickListener(this);
-        tv_mail.setOnClickListener(this);
-        tv_web.setOnClickListener(this);
-        tv_follow_us.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.iv_share:
-            case R.id.tv_share:
-                Intent sendIntent = new Intent();
-                sendIntent.setAction(Intent.ACTION_SEND);
-                sendIntent.putExtra(Intent.EXTRA_TEXT, "FMC");
-                sendIntent.setType("text/plain");
-                startActivity(sendIntent);
-                break;
-            case R.id.iv_web:
-            case R.id.tv_web:
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com")));
-                break;
-            case R.id.iv_mail:
-            case R.id.tv_mail:
-                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
-                        "mailto", "m.srinivas91@gmail.com", null));
-                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "FM Council");
-                startActivity(Intent.createChooser(emailIntent, "Send email..."));
-                break;
-            case R.id.iv_mobile:
-            case R.id.tv_mobile_num:
-                Intent intentDail = new Intent(Intent.ACTION_DIAL);
-                intentDail.setData(Uri.parse("tel:"+tv_mobile_num.getText().toString().trim()));
-                startActivity(intentDail);
-                break;
-            case R.id.iv_fb:
-                String uri = "https://www.facebook.com/versatile.mobitech/";
-                Intent fbIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-                startActivity(fbIntent);
-                break;
-            case R.id.iv_g_plus:
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://plus.google.com/105152463775757641701")));
-                break;
-            case R.id.iv_linked_in:
-                Intent linkedIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://in.linkedin.com/in/versatilemobitech"));
-                final PackageManager packageManager = getContext().getPackageManager();
-                final List<ResolveInfo> list = packageManager.queryIntentActivities(linkedIntent, PackageManager.MATCH_DEFAULT_ONLY);
-                if (list.isEmpty()) {
-                    linkedIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://in.linkedin.com/in/versatilemobitech"));
-                }
-                startActivity(linkedIntent);
-                break;
-            case R.id.iv_twitter:
-                String mTwitterUrl = "https://twitter.com/VMobitech";
-                try {
-                    Intent twitterIntent = new Intent(Intent.ACTION_VIEW,
-                            Uri.parse(mTwitterUrl));
-                    startActivity(twitterIntent);
-
-                } catch (Exception e) {
-                    startActivity(new Intent(Intent.ACTION_VIEW,
-                            Uri.parse(mTwitterUrl)));
-                }
-                break;
-        }
-    }
     private void CheckForPermissions(final Context mContext, final String... mPermisons) {
         // A request for two permissions
         Permissions.getInstance().requestPermissions(new Permissions.IOnPermissionResult() {
@@ -219,5 +95,135 @@ public class ContactsUsFragment extends Fragment implements View.OnClickListener
                         Permissions.MESSAGE, "RE-TRY", callback);
             }
         }, mPermisons);
+    }
+
+    private void InitUI(View rootView) {
+
+        TextView tv_contact_us = (TextView) rootView.findViewById(R.id.tv_contact_us);
+        tv_contact_us.setTypeface(Utility.setTypeFaceRobotoBold(getActivity()));
+
+        TextView tv_address = (TextView) rootView.findViewById(R.id.tv_address);
+        tv_address.setTypeface(Utility.setTypeFaceRobotoRegular(getActivity()));
+
+        TextView tv_phone = (TextView) rootView.findViewById(R.id.tv_phone);
+        tv_phone.setTypeface(Utility.setTypeFaceRobotoRegular(getActivity()));
+        tv_phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "9247103736"));
+                startActivity(intent);
+            }
+        });
+
+        TextView tv_mail = (TextView) rootView.findViewById(R.id.tv_mail);
+        tv_mail.setTypeface(Utility.setTypeFaceRobotoRegular(getActivity()));
+        tv_mail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                        "mailto", "fmcouncil@gmail.com", null));
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "FM Council");
+                startActivity(Intent.createChooser(emailIntent, "Send email..."));
+            }
+        });
+
+        TextView tv_site = (TextView) rootView.findViewById(R.id.tv_site);
+        tv_site.setTypeface(Utility.setTypeFaceRobotoRegular(getActivity()));
+        tv_site.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.ourtour.in";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+
+        TextView tv_share = (TextView) rootView.findViewById(R.id.tv_share);
+        tv_share.setTypeface(Utility.setTypeFaceRobotoRegular(getActivity()));
+
+        TextView tv_follow_us = (TextView) rootView.findViewById(R.id.tv_follow_us);
+        tv_follow_us.setTypeface(Utility.setTypeCambriaBoldRegular(getActivity()));
+
+        ImageView iv_yogi = (ImageView) rootView.findViewById(R.id.iv_logo);
+        ImageView iv_address = (ImageView) rootView.findViewById(R.id.iv_address);
+        ImageView iv_phone = (ImageView) rootView.findViewById(R.id.iv_phone);
+        ImageView iv_mail = (ImageView) rootView.findViewById(R.id.iv_mail);
+        ImageView iv_site = (ImageView) rootView.findViewById(R.id.iv_site);
+
+        LinearLayout ll_share = (LinearLayout) rootView.findViewById(R.id.ll_share);
+
+        ImageView iv_fb = (ImageView) rootView.findViewById(R.id.iv_fb);
+        iv_fb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uri = "https://www.facebook.com/versatile.mobitech/";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                startActivity(intent);
+            }
+        });
+
+        ImageView iv_twitter = (ImageView) rootView.findViewById(R.id.iv_twitter);
+        iv_twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mTwitterUrl = "https://twitter.com/VMobitech";
+                try {
+                    Intent intent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse(mTwitterUrl));
+                    startActivity(intent);
+
+                } catch (Exception e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW,
+                            Uri.parse(mTwitterUrl)));
+                }
+            }
+        });
+        ImageView iv_google = (ImageView) rootView.findViewById(R.id.iv_google);
+        iv_google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://plus.google.com/105152463775757641701")));
+            }
+        });
+      /*  ImageView iv_you_tube = (ImageView) rootView.findViewById(R.id.iv_you_tube);
+        iv_you_tube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //you tube link
+            }
+        });*/
+        ImageView iv_share = (ImageView) rootView.findViewById(R.id.iv_share);
+        iv_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "New version of FMC is available. " + "https://play.google.com/store/apps/details?id=com.versatilemobitech.fmc&hl=en");
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+            }
+        });
+        tv_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "New version of FMC is available. " + "https://play.google.com/store/apps/details?id=com.versatilemobitech.fmc&hl=en");
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+            }
+        });
+        ll_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "New version of FMC is available. " + "https://play.google.com/store/apps/details?id=com.versatilemobitech.fmc&hl=en");
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+            }
+        });
+
     }
 }
